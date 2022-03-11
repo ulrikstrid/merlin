5ec07bf6daac6cdaf193c159ee5cb1b2244729ee
Ulysse Gérard <thevoodoos@gmail.com>
2022-03-11 12:13:55 +0100

    Remove old protocol

--- src/frontend/ocamlmerlin/ocamlmerlin_server.ml
+++ src/frontend/ocamlmerlin/ocamlmerlin_server.ml
@@ -74,22 +74,18 @@ let main () =
   Unix.putenv "__MERLIN_MASTER_PID" (string_of_int (Unix.getpid ()));
   match List.tl (Array.to_list Sys.argv) with
   | "single" :: args -> exit (New_merlin.run ~new_env:None None args)
-  | "old-protocol" :: args -> Old_merlin.run args
   | ["server"; socket_path; socket_fd] -> Server.start socket_path socket_fd
   | ("-help" | "--help" | "-h" | "server") :: _ ->
     Printf.eprintf
       "Usage: %s <frontend> <arguments...>\n\
        Select the merlin frontend to execute. Valid values are:\n\
-      \n- 'old-protocol' executes the merlin frontend from previous version.\n\
-      \  It is a top level reading and writing commands in a JSON form.\n\
       \n- 'single' is a simpler frontend that reads input from stdin,\n\
       \  processes a single query and outputs result on stdout.\n\
       \n- 'server' works like 'single', but uses a background process to\n\
       \  speedup processing.\n\
-       If no frontend is specified, it defaults to 'old-protocol' for\n\
-       compatibility with existing editors.\n"
+       If no frontend is specified, it defaults to 'single'.\n"
       Sys.argv.(0)
-  | args -> Old_merlin.run args
+  | args -> exit (New_merlin.run ~new_env:None None args)
 
 let () =
   let log_file =
